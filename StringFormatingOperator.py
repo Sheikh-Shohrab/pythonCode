971ed16adc2db029c5afb9d5c9c1bdb0722eb92e
sheikh-shohrab <cseshohrab.cu@gmail.com>
2024-11-24 10:41:50 +0600

    add the string modifyer

--- StringFormatingOperator.py
+++ StringFormatingOperator.py
@@ -1,3 +1,8 @@
 name = "MD. SHOHRAB HOSSAIN"
 ID = "2025402003"
+
+message = "Hi 'MD. SHOHRAB HOSSAIN' You are so lucy person."
+pessage = 'Hi "Robel" You are so clebar boy'
+
 print("NAME: %s \nYour ID: %s" %(name, ID))
+print(message, "\n", pessage)
