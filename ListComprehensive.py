40978be4f55ae974cd4040200d364002e097bfc8
sheikh-shohrab <cseshohrab.cu@gmail.com>
2024-11-26 11:16:22 +0600

    sets operation

---
+++ ListComprehensive.py
@@ -0,0 +1,4 @@
+myList = [10,20,30,40,50]
+myListSqrt = [i**2 for i in myList]
+print(myList)
+print(myListSqrt)
---
+++ SetInPython.py
@@ -0,0 +1,5 @@
+mySet = {1,2,3,4,5,6,7,8,9,10,11}
+
+evenSet = {x for x in mySet if x%2==0}
+print({x for x in mySet if x%2==0})
+print(evenSet)
